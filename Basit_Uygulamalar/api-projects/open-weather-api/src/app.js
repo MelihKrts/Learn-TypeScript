d710fe8b6c652591b466941252f2aeac5ecaa2d0
MelihKrts <melih.karatas1281@gmail.com>
2025-01-17 21:27:36 +0300

    fix: update and secure API keys for weather-api

--- Basit_Uygulamalar/api-projects/open-weather-api/src/app.js
+++ Basit_Uygulamalar/api-projects/open-weather-api/src/app.js
@@ -1,4 +1,4 @@
-import { OPENWEATHER_API_KEY } from "./key.js";
+import { OPENWEATHER_API_KEY } from "./key";
 let cityInput = document.getElementById("city-input");
 let searchBtn = document.getElementById("search-btn");
 let cityNameElement = document.getElementById("city-name");
@@ -17,26 +17,20 @@ const searchWeather = async () => {
             throw new Error("Şehir bulunamadı. Lütfen doğru bir şehir adı girin.");
         }
         const data = await response.json();
-        // Şehir ve Ülke
         cityNameElement.textContent = `${data.name}, ${data.sys.country}`;
-        // Sıcaklık
         temperatureElement.textContent = `Sıcaklık: ${data.main.temp.toFixed(1)}°C`;
-        // Hava Durumu
         weatherCondition.textContent = `Durum: ${capitalizeFirstLetter(data.weather[0].description)}`;
-        // Giriş alanını temizle
         cityInput.value = "";
-        console.log(data); // Konsola veri yazdırma (isteğe bağlı)
+        console.log(data);
     }
     catch (error) {
         console.error(error);
         alert("Hava durumu bilgileri alınamadı. Lütfen tekrar deneyin.");
     }
 };
-// Hava durumu açıklamasının ilk harfini büyük yapmak için bir yardımcı fonksiyon
 const capitalizeFirstLetter = (text) => {
     return text.charAt(0).toUpperCase() + text.slice(1);
 };
-// Olay dinleyiciler
 searchBtn.addEventListener("click", searchWeather);
 cityInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
